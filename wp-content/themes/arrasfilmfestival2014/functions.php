cdb823123c31c89a255266af03b2aaca84f42384
Thomas Leduc <thomas.leduc@epsi.fr>
2014-07-12 22:36:26 +0200

    Menu classic & responsive add for all pages

--- wp-content/themes/arrasfilmfestival2014/functions.php
+++ wp-content/themes/arrasfilmfestival2014/functions.php
@@ -2,6 +2,7 @@
 /* Theme settings */
 setlocale(LC_ALL, 'fr_FR');
 add_theme_support('post-thumbnails');
+add_theme_support('menus');
 
 /* Custom Post Type */
 function custom_post_type()
@@ -99,3 +100,44 @@ function remove_menus()
   	remove_menu_page( 'edit-comments.php' );
 }
 add_action( 'admin_menu', 'remove_menus' );
+
+class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
+	function start_el ( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$class_names = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$atts = array();
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+		$item_output = $args->before;
+		$item_output .= '<a '. $class_names .' '. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+}
--- wp-content/themes/arrasfilmfestival2014/header.php
+++ wp-content/themes/arrasfilmfestival2014/header.php
@@ -1,3 +1,19 @@
+<?php
+$menu_settings = array(
+	'container'		=> false,
+	'echo'			=> false,
+	'menu_class'	=> 'yolo',
+	'walker' 		=> new Custom_Walker_Nav_Menu
+);
+
+$responsive_menu_settings = array(
+	'menu'			=> 'principal responsive',
+	'container'		=> false,
+	'echo'			=> false,
+	'menu_class'	=> 'yolo',
+	'walker' 		=> new Custom_Walker_Nav_Menu
+);
+?>
 <!DOCTYPE html>
 <html>
 	<head <?php language_attributes(); ?>>
@@ -10,4 +26,10 @@
 		<header>
 			<h1><?php bloginfo('name'); ?></h1>
 			<h2><?php bloginfo('description'); ?></h2>
+			<nav>
+			<?php echo strip_tags(wp_nav_menu($menu_settings), '<a>'); ?>	
+			</nav>
+			<nav>
+			<?php echo strip_tags(wp_nav_menu($responsive_menu_settings), '<a>'); ?>	
+			</nav>
 		</header>
