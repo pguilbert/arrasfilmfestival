f90cbe9482fb2b9d4475b7d49e03d85ccd57e18a
Thomas Leduc <thomas.leduc@epsi.fr>
2014-07-20 17:49:24 +0200

    remove everything but the theme

--- wp-content/themes/arrasfilmfestival2014/404.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-<p class="_404-paragraph">Oh mince ! :( Il semblerait que la page que vous désirez n’existe pas ou plus.&nbsp;</p>
-<h1 class="_404-title">404.</h1>
-<?php get_footer(); ?>
