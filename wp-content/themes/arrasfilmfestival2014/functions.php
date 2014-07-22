0da5aaaa93b79c4afee863cbae32ead72df2bbe3
Thomas Leduc <thomas.leduc@epsi.fr>
2014-07-22 20:46:00 +0200

    Set permissions to users's role

--- wp-content/themes/arrasfilmfestival2014/functions.php
+++ wp-content/themes/arrasfilmfestival2014/functions.php
@@ -5,6 +5,7 @@ require_once('includes/parameters.php');
 require_once('includes/responsive_parameters.php');
 require_once('includes/picture_size.php');
 require_once('includes/helpers.php');
+require_once('includes/capabilities.php');
 
 require_once('includes/class/Custom_Walker_Nav_Menu.php');
 require_once('includes/class/Custom_Walker_Category.php');
