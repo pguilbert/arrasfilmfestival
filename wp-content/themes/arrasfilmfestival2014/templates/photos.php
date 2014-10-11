e850b66817247709780303e88194f443b2f9821b
tomapp <thomas.leduc@epsi.fr>
2014-10-11 19:43:50 +0200

    Distorting problem : Apply same logic to photo template

--- wp-content/themes/arrasfilmfestival2014/templates/photos.php
+++ wp-content/themes/arrasfilmfestival2014/templates/photos.php
@@ -11,19 +11,33 @@ get_header();
 	<h1 class="responsive-margin-left">Le festival en image</h1>
 	<div id="gallery">
 	<?php foreach($recent_pictures->posts as $picture): ?>
-		<?php $random = random_size(array(376, 574)); ?>
 		<?php 
 		if(empty($picture->post_excerpt)) {
 			$title = 'Publié par ' . get_the_author_meta('first_name', $picture->post_author). ' ' . get_the_author_meta('last_name', $picture->post_author);
 		} else {
 			$title = $picture->post_excerpt . ' - ' . get_the_author_meta('first_name', $picture->post_author) . ' ' . get_the_author_meta('last_name', $picture->post_author);
 		}
+
+		$random = random_size(array(376, 574)); 
+		$pictureUrl = wp_get_attachment_image_src($picture->ID, '376x' . $random)[0]; 
+
+		$size = getimagesize(str_replace('http://' . $_SERVER['HTTP_HOST'] . '/', '', $pictureUrl));
 		?>
 		<div class="item">
 			<a title="<?php echo $title; ?>" class="w-inline-block thumbnail-link fancybox" rel="Galerie" href="<?php echo wp_get_attachment_image_src($picture->ID, '800x600')[0]; ?>">
-				<img class="thumbnail" src="<?php echo wp_get_attachment_image_src($picture->ID, '376x' . $random)[0]; ?>" width="178" height="<?php echo $random / 2; ?>">
-			</a>
-		</div>
+			<?php
+			if($size[1] < $random) :
+				$newHeight = (178 / $size[0]) * $size[1];
+	        ?>
+				<img class="thumbnail" src="<?php echo $pictureUrl; ?>" width="178" height="<?php echo $newHeight; ?>">
+	        <?php
+	        else :
+	        ?>
+				<img class="thumbnail" src="<?php echo $pictureUrl; ?>" width="178" height="<?php echo $random / 2; ?>">
+	        <?php
+	        endif;
+	        ?>
+        </div>
 	<?php endforeach; ?>
 	</div>
 </div>
